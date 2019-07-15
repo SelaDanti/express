f980ba4b78a6b872d98df8bf4cd4ce03662b6f17
seladanti <kwameasiago@gmail.com>
2019-07-15 10:59:44 +0300

    middleware

--- index.js
+++ index.js
@@ -3,7 +3,15 @@ var app = express();
 
 var things =  require('./things');
 
+app.use((req, res, next) => {
+console.log('first middleware')
+next()
+})
 app.use('/things', things);
+
+app.use('/things', (req,res, next) => {
+    console.log('end')
+})
 
 app.listen(3000, () => {
 console.log('app running on port 3000')
